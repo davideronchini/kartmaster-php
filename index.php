258b94e201db25f0366233ac8157405e4cccc142
Davide <81747125+davideronchini@users.noreply.github.com>
2022-12-12 18:09:38 +0100

    Moved javascript code to redirect the user to app-login.php page

--- index.php
+++ index.php
@@ -20,6 +20,18 @@
     <link rel="stylesheet" href="./static/css/style-index.css">
     <link rel="shortcut icon" href="./static/resources/favicon.webp">
 
+    <script type="text/javascript">
+
+      //Android settings
+      var ua = navigator.userAgent.toLowerCase();
+      var isAndroid = ua.indexOf("android") > -1 && ua.indexOf("mobile");
+
+      if(isAndroid) {
+        window.location.href = "./app-login.php";
+      }
+
+    </script>
+
   </head>
   <body>
 
@@ -140,17 +152,6 @@
 
     <!-- Link to js file -->
     <script src="./static/js/script.js"></script>
-    <script type="text/javascript">
-
-      //Android settings
-      var ua = navigator.userAgent.toLowerCase();
-      var isAndroid = ua.indexOf("android") > -1 && ua.indexOf("mobile");
-
-      if(isAndroid) {
-        window.location.href = "./app-login.php";
-      }
-
-    </script>
     
 </body>
 </html>
